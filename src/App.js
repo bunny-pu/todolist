abedc3ccf18363f1679906eeebbf41067a77caf4
gegeding <gege.ding@shopee.co>
2021-10-20 14:53:17 +0800

    初步实现静态react页面

--- src/App.js
+++ src/App.js
@@ -3,21 +3,28 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="total">
+        <input className="addtodo" id="f55" placeholder="请输入代办事项" onkeydown="fun3(event)"/>
+            <div className="list"  id="lll">
+                <form action="/action_page.php" id="iii">
+                    <input type="checkbox" id="vehicle1" name="vehicle1" value="Bike" onmouseover = "func()"  onmouseout = "func1()"/>
+                    <label id="dff">I have a bike</label>
+                    <a id="ppp">删除此项待办ddd</a>
+                    <br/><hr/>
+                    <input type="checkbox" id="vehicle2" name="vehicle2" value="Car"/>
+                    <label> I have a car</label>
+                    <a id="ppp">删除此项待办</a>
+                    <br/>
+                    <hr/>
+                    <input type="checkbox" id="vehicle3" name="vehicle3" value="Boat"/>
+                    <label> I have a boat</label>
+                    <a id="ppp">删除此项待办</a>
+                    <br/>
+                </form>
+            </div>
+
+        <input className="sub" type="checkbox" id="sub_todos" onclick="checkAll(this)"/>
+                <label for="vehicle1" id="vehicletext"> 已完成0/全部3</label><br/>
     </div>
   );
 }
